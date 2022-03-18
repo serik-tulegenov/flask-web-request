1fa2a1153abd9721e7da3c1f58c0ef0f6fdbb01b
serik-tulegenov <tulegenoff.s@gmail.com>
2022-03-18 09:50:36 +0000

    Created note templates

--- __init__.py
+++ __init__.py
@@ -1,6 +1,7 @@
 import os
+import functools
 
-from flask import Flask, render_template, redirect, url_for, request, session, flash
+from flask import Flask, render_template, redirect, url_for, request, session, flash, g
 from flask_migrate import Migrate
 from werkzeug.security import generate_password_hash, check_password_hash
 
@@ -16,10 +17,27 @@ def create_app(test_config=None):
     else:
         app.config.from_mapping(test_config)
 
-    from .models import db, User
+    from .models import db, User, Note
 
     db.init_app(app)
     migrate = Migrate(app, db)
+
+    def require_login(view):
+        @functools.wraps(view)
+        def wrapped_view(**kwargs):
+            if not g.user:
+                return redirect(url_for('log_in'))
+            return view(**kwargs)
+        return wrapped_view
+
+    @app.before_request
+    def load_user():
+        user_id = session.get('user_id')
+        if user_id:
+            g.user = User.query.get(user_id)
+        else:
+            g.user = None   
+
 
     @app.route('/sign_up', methods=('GET', 'POST'))
     def sign_up():
@@ -47,7 +65,7 @@ def create_app(test_config=None):
 
         return render_template('sign_up.html')
 
-    @app.route('/log_in')
+    @app.route('/log_in', methods=('GET','POST'))
     def log_in():
         if request.method == 'POST':
             username = request.form['username']
@@ -58,12 +76,12 @@ def create_app(test_config=None):
 
             if not user or not check_password_hash(user.password, password):
                 error = 'Username or password are incorrect'
-            if error in None:
+            if error is None:
                 session.clear()
                 session['user_id'] = user.id
                 return redirect(url_for('index'))
             
-            flash(error, category='error')
+            flash(error, 'error')
         return render_template('log_in.html')
 	
     @app.route('/')
@@ -75,7 +93,30 @@ def create_app(test_config=None):
         session.clear()
         flash('Successfuly logged out.', 'success')
         return redirect(url_for('log_in'))
+    
+    @app.route('/notes')
+    @require_login
+    def note_index():
+        return render_template('note_index.html', notes=g.user.notes)
+    
+    @app.route('/notes/new', methods=('GET', 'POST'))
+    @require_login
+    def note_create():
+        if request.method == 'POST':
+            title = request.form['title']
+            body = request.form['body']
+            error = None
 
+            if not title:
+                error = 'Title is required.'
+            if not error:
+                note = Note(author=g.user, title=title, body=body)
+                db.session.add(note)
+                db.session.commit()
+                flash(f"Successfuly create note: '{title}'", 'success')
+                return redirect(url_for('note_index'))
+            flash(error, 'error')
+        return render_template('note_create.html')
 
     return app
 
